e9a2712c778b8f427ae7ccd4d2a053389771b4ab
Luis Alejandro Sanchez <118697122+Alejosanch97@users.noreply.github.com>
2026-04-23 15:35:40 +0000

    styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'  // Global styles for your application
+import "./Styles/footer.css"; 
+import "./Styles/navbar.css";
+import './Styles/whatsapp.css';
+import "./Styles/home.css"; 
+import "./Styles/proyectos.css"; 
+import "./Styles/servicios.css"; 
+
 import { RouterProvider } from "react-router-dom";  // Import RouterProvider to use the router
 import { router } from "./routes";  // Import the router configuration
 import { StoreProvider } from './hooks/useGlobalReducer';  // Import the StoreProvider for global state management
